8fb27c24a2b7b42f182c2f874037a0136db233a9
Dina Gomes <dinafgomes@gmail.com>
2015-07-14 19:37:37 +0100

    Final code
    as it has been presented

---
+++ PelicanCross/MainPage.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PelicanCross.Resources;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace PelicanCross
+{
+
+    public partial class MainPage : PhoneApplicationPage, INotifyPropertyChanged
+    {
+        // Data context for the local database
+        public DataContext gameDB;
+        
+               
+        // Define an observable collection property that controls can bind to.
+        public ObservableCollection<GameTable> _playersList;
+
+        public ObservableCollection<GameTable> PlayersList{
+            get { return _playersList; }
+            set{if (_playersList != value)
+                {_playersList = value;
+                 NotifyPropertyChanged("PlayersList");}}}
+        
+
+        // Constructor
+        public MainPage(){
+
+            InitializeComponent();
+            Application.Current.Host.Settings.EnableFrameRateCounter = false;
+
+
+            
+	        // Connect to the database and instantiate data context.
+            gameDB = new GameDataContext(GameDataContext.DBConnectionString);
+            this.DataContext = this;
+            }
+
+        
+            //UI Event Handlers
+            
+            private void newGameAddButton_Click(object sender, RoutedEventArgs e){
+                NavigationService.Navigate(new Uri("/TrafficLights.xaml", UriKind.Relative));
+
+                //addPlayer(newGameTextBox.Text, 0);
+                App pname = Application.Current as App;
+                pname.PName = newGameTextBox.Text;
+
+                App ppoints = Application.Current as App;
+                ppoints.PPoints = 0;
+                
+                
+            }
+                
+            private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+            {
+                App pname = Application.Current as App;
+                App ppoints = Application.Current as App;
+                if(ppoints.PPoints > 0)
+                {
+                    addPlayer(pname.PName, ppoints.PPoints);
+                
+                }
+            }
+        
+           public void addPlayer(string name, double points){
+                //Adding new player to the list
+                GameTable newPlayer = new GameTable { PlayerName = name, PlayerPoints = points };
+                PlayersList.Add(newPlayer);
+     
+                gameDB.GetTable<GameTable>().InsertOnSubmit(newPlayer);
+                
+                
+                
+            }
+
+            
+            
+            private void newGameTextBox_GotFocus(object sender, RoutedEventArgs e){
+                newGameTextBox.Text = String.Empty;}
+
+            private void deletePlayerButton_Click(object sender, RoutedEventArgs e){
+
+                var button = sender as Button;
+
+                if (button != null)
+                {
+
+                    GameTable playerForDelete = button.DataContext as GameTable;
+                    PlayersList.Remove(playerForDelete);
+                    gameDB.GetTable<GameTable>().DeleteOnSubmit(playerForDelete);
+                    gameDB.SubmitChanges();
+                    this.Focus();
+                }
+            }
+
+            protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+            { base.OnNavigatedFrom(e); gameDB.SubmitChanges(); }
+
+            protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e){
+                var PlayersInDB = from GameTable players in gameDB.GetTable<GameTable>() select players;                
+                PlayersList = new ObservableCollection<GameTable>(PlayersInDB);
+               
+                base.OnNavigatedTo(e);}
+
+            #region INotifyPropertyChanged Members
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            // Used to notify the app that a property has changed.
+            private void NotifyPropertyChanged(string propertyName)
+            {if (PropertyChanged != null) {PropertyChanged(this, new PropertyChangedEventArgs(propertyName));}}
+
+            #endregion
+    }
+
+        public class GameDataContext : DataContext{
+            public static string DBConnectionString = "DataSource=isostore:/Game.sdf";
+            public GameDataContext(string connectionString): base(connectionString){ }
+            public Table<GameTable> PlayersList;}
+    
+        [Table] public class GameTable : INotifyPropertyChanged, INotifyPropertyChanging{
+
+            private int _playerId;
+            private string _playerName;
+            private double _playerPoints;
+            
+            [Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identity", CanBeNull = false, AutoSync = AutoSync.OnInsert)]
+            
+            public int PlayerId{
+                get { return _playerId; }
+                set{if (_playerId != value){
+                    NotifyPropertyChanging("PlayerId");
+                    _playerId = value;
+                    NotifyPropertyChanged("PlayerId");}}}
+
+            [Column] public string PlayerName{
+                get { return _playerName; }
+                set{if (_playerName != value){
+                    NotifyPropertyChanging("PlayerName");
+                    _playerName = value;
+                    NotifyPropertyChanged("PlayerName");}}}
+
+            [Column] public double PlayerPoints{
+                get { return _playerPoints; }
+                set{if (_playerPoints != value){
+                    NotifyPropertyChanging("PlayerPoints");
+                    _playerPoints = value;
+                    NotifyPropertyChanged("PlayerPoints");}}}
+
+            #region INotifyPropertyChanged Members
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private void NotifyPropertyChanged(string propertyName)
+            {if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); }}
+
+            #endregion
+
+            #region INotifyPropertyChanging Members
+
+            public event PropertyChangingEventHandler PropertyChanging;
+
+            private void NotifyPropertyChanging(string propertyName)
+            {if (PropertyChanging != null){PropertyChanging(this, new PropertyChangingEventArgs(propertyName));}}
+
+            #endregion
+            
+        }
+
+}
---
+++ PelicanCross/TrafficLights.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Devices.Sensors;
+using Microsoft.Xna.Framework;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+
+namespace PelicanCross
+{
+    
+    public partial class TrafficLights : PhoneApplicationPage
+    {
+
+        Accelerometer accelerometer;
+        int i = 0;
+        public int points = 0;
+        public double points = 0;
+
+     
+
+        //Constructor
+        public TrafficLights()
+        {
+ 
+
+            InitializeComponent();
+        
+            //Accelerometer
+            if(!Accelerometer.IsSupported){MessageBox.Show("No accelerometer found");}
+            if (accelerometer == null){
+                accelerometer = new Accelerometer();
+                accelerometer.TimeBetweenUpdates = TimeSpan.FromSeconds(1);
+                accelerometer.CurrentValueChanged += new EventHandler<SensorReadingEventArgs<AccelerometerReading>>(accelerometerEventHandler);
+                accelerometer.Start();}}
+        
+            private void accelerometerEventHandler(object sender, SensorReadingEventArgs<AccelerometerReading> e){
+                Dispatcher.BeginInvoke(()=>UpdateUI(e.SensorReading));}
+
+
+            public void UpdateUI(AccelerometerReading accelerometerReading){
+                Vector3 accelerator = accelerometerReading.Acceleration;
+                points = points + (accelerator.Z+accelerator.X) + 1;
+                readings.Text = "Speed: " + points + "\nTimer: " + i++.ToString();
+            
+                Brush brushRed      = new SolidColorBrush(Colors.Red);
+                Brush brushAmber    = new SolidColorBrush(Colors.Yellow);
+                Brush brushGreen    = new SolidColorBrush(Colors.Green);
+                Brush brushGray     = new SolidColorBrush(Colors.Gray);
+            
+                //Start Red
+                if (i == 1){
+                    amberLight.Fill = brushGray;
+                    redLight.Fill = brushRed;
+                    greenLight.Fill = brushGray;}
+                //In 2 seconds Go Green
+                 if (i == 2){
+                    amberLight.Fill = brushGray;
+                    redLight.Fill   = brushGray;
+                    greenLight.Fill = brushGreen;}
+                 //In 4 seconds Go Amber
+                if (i == 3){
+                    amberLight.Fill = brushAmber;
+                    redLight.Fill = brushGray;
+                    greenLight.Fill = brushGray;}
+                //In 6 seconds Go Red again
+                if(i==4){
+                    redLight.Fill = brushRed;
+                    greenLight.Fill = brushGray;
+                    amberLight.Fill = brushGray;} 
+                //Wait 1 second and give the score
+                if (i == 6){
+                    accelerometer.Stop();
+                    if (Math.Round(points) > 0)
+                    {
+                        MessageBox.Show("You made " + Math.Round(points) + " points!");
+                       NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+                        //points = Math.Round(points);
+                        App ppoints = Application.Current as App;
+                        ppoints.PPoints = Math.Round(points);
+
+                    }
+                    else{
+                        MessageBox.Show("Oh No, you didn't cross the street correctly. Player disclassified, (" + points + " points!)");
+                       NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+                    }
+                    }
+                
+               }
+            //End Accelerometer      
+                  
+
+        }
+    }
